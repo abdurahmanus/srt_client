bedd5a1c426a129b9a4a0ed10242bfc866323cb1
Dmitry Gladkikh <abdurahmanus@gmail.com>
2017-12-22 14:45:00 +0300

    add some immutable tests

--- src/immutable.test.js
+++ src/immutable.test.js
@@ -8,7 +8,10 @@ it('Map merges with plain js object correctly', () => {
     })
     const m2 = m1.merge({
         key3: {
-            key4: 'some value 4'
+            key4: 'some value 4',
+            key5: {
+                key6: 'some value 6'
+            }
         }
     })
     expect(m2).toEqual(Map({
@@ -16,7 +19,30 @@ it('Map merges with plain js object correctly', () => {
             key2: 'some value 2'
         }),
         key3: Map({
-            key4: 'some value 4'
+            key4: 'some value 4',
+            key5: Map({
+                key6: 'some value 6'
+            })
+        })
+    }))
+})
+
+it('Map setIn with plain js object correctly', () => {
+    const m1 = fromJS({
+        k1: {
+            k2: {
+                k3: 'value 3'
+            }
+        }
+    })
+    const m2 = m1.setIn(['k1', 'k2'], fromJS({
+        k99: 'value 99'
+    }))
+    expect(m2).toEqual(Map({
+        k1: Map({
+            k2: Map({
+                k99: 'value 99'
+            })
         })
     }))
 })
